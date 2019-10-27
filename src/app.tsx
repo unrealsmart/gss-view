f5a57646b9170d20f75b7c133263357ce6ee437b
Administrator <354148728@qq.com>
2019-10-28 00:23:23 +0800

    add loading elements

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,6 @@ function fetchADPConfig(oldRender: Function): void {
   request('main/all-config/adp').then(data => {
     if (data) {
       localStorage.setItem('all-config-adp', JSON.stringify(data));
-      ReactDOM.render(<></>, document.getElementById('loading'));
       oldRender();
     } else {
       ReactDOM.render(<Termination error={data} />, document.getElementById('root'));
@@ -17,6 +16,7 @@ function fetchADPConfig(oldRender: Function): void {
 
 export function render(oldRender: Function): void {
   request('main/ping').then(data => {
+    ReactDOM.render(<></>, document.getElementById('loading'));
     if (data instanceof Response) {
       ReactDOM.render(<Termination error={data} />, document.getElementById('root'));
     } else {
