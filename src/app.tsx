c86635ca832ace8d0c2887f0e3075c856ca14cc9
Administrator <354148728@qq.com>
2019-10-31 12:14:23 +0800

    modify request url

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import request from '@/utils/request';
 import Termination from '@/components/Termination';
 
 function fetchADPConfig(oldRender: Function): void {
-  request('main/all-config/adp').then(data => {
+  request('/main/all-config/adp').then(data => {
     if (data) {
       localStorage.setItem('all-config-adp', JSON.stringify(data));
       oldRender();
@@ -15,7 +15,7 @@ function fetchADPConfig(oldRender: Function): void {
 }
 
 export function render(oldRender: Function): void {
-  request('main/ping').then(data => {
+  request('/main/ping').then(data => {
     ReactDOM.render(<></>, document.getElementById('loading'));
     if (data instanceof Response) {
       ReactDOM.render(<Termination error={data} />, document.getElementById('root'));
